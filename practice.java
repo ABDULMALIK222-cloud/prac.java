19dbcf00e562d57a30afbc62ad7a7388e09c3a8f
Abdul Malik <128873080+ABDULMALIK222-cloud@users.noreply.github.com>
2024-12-20 23:30:51 +0530

    Update practice.java
    ..

--- practice.java
+++ practice.java
@@ -16,3 +16,95 @@ class Armstrong{
     }
   }
 }
+// program on inheritance like inherit the method from another class
+class Animal{
+    void eat(){
+        System.out.println("Animals often eat to survive");
+    }
+}
+class Cat extends Animal{
+    void sound(){
+        System.out.println("Cats says meow!");
+    }
+}
+class Fox extends Animal{
+    void eyes(){
+        System.out.println("cat eyes shines in the night like ghost");
+    }
+}
+class Bear extends Animal{
+    void skin(){
+        System.out.println("cat skin is hairy");
+    }
+}
+class Lion extends Animal{
+    void nails(){
+        System.out.println("nails are very sharp");
+    }
+}
+class Dog extends Animal{
+    void sound(){
+        System.out.println("dogs  barks");
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Cat cat = new Cat();
+        Fox fox = new Fox();
+        Lion lion = new Lion();
+        Bear bear = new Bear();
+        Dog dog = new Dog();
+        cat.eat();
+        cat.sound();
+        fox.eyes();
+        bear.skin();
+        lion.nails();
+        dog.sound();
+    }
+}
+// program on polymorphism
+class Classic{
+  void complaints(){
+    System.out.println("waste hostel");
+  }
+}
+class Food extends Classic{
+  void complaints(){
+    System.out.println("waste food hostel");
+  }
+}
+class Parking extends Classic{
+  void complaints(){
+    System.out.println("No parking in  hostel");
+  }
+}
+class Rooms extends Classic{
+  void complaints(){
+    System.out.println("no clean rooms in hostel");
+  }
+}
+class Water extends Classic{
+  void complaints(){
+    System.out.println("no proper water in hostel");
+  }
+}
+class Management extends Classic{
+  void complaints(){
+    System.out.println("waste management in  hostel");
+  }
+}
+public class Main{
+  public static void main(String[] args){
+    Classic food = new Food();
+    Classic parking = new Parking();
+    Classic rooms = new Rooms();
+    Classic water = new Water();
+    Classic management = new Management();
+    food.complaints();
+    parking.complaints();
+    rooms.complaints();
+    water.complaints();
+    management.complaints();
+  }
+}
+    
